07166951dc5cdf1e0844475e495a8f023ce1121a
Aaron Rutledge <AaronHRutledge@GMail.com>
2019-08-14 11:31:23 -0600

    Updating shim.  adding randmobytes and added keystore for publishing

--- shim.js
+++ shim.js
@@ -1,9 +1,9 @@
-if (typeof __dirname === 'undefined') global.__dirname = '/';
-if (typeof __filename === 'undefined') global.__filename = '';
-if (typeof process === 'undefined') {
-  global.process = require('process');
+if (typeof __dirname === "undefined") global.__dirname = "/";
+if (typeof __filename === "undefined") global.__filename = "";
+if (typeof process === "undefined") {
+  global.process = require("process");
 } else {
-  const bProcess = require('process');
+  const bProcess = require("process");
   for (const p in bProcess) {
     if (!(p in process)) {
       process[p] = bProcess[p];
@@ -12,22 +12,42 @@ if (typeof process === 'undefined') {
 }
 
 process.browser = false;
-if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer;
+if (typeof Buffer === "undefined") global.Buffer = require("buffer").Buffer;
 
 // global.location = global.location || { port: 80 }
-const isDev = typeof __DEV__ === 'boolean' && __DEV__;
-process.env['NODE_ENV'] = isDev ? 'development' : 'production';
-if (typeof localStorage !== 'undefined') {
-  localStorage.debug = isDev ? '*' : '';
+const isDev = typeof __DEV__ === "boolean" && __DEV__;
+process.env["NODE_ENV"] = isDev ? "development" : "production";
+if (typeof localStorage !== "undefined") {
+  localStorage.debug = isDev ? "*" : "";
 }
 
 // Make sure crypto gets loaded first, so it can populate global.crypto
-if (require('./package.json').dependencies['react-native-crypto']) {
-  // Placing the module name in a variable prevents 'crypto' from being
-  // pre-loaded (which could cause an error, since it may not exist)
-  require('crypto');
-}
+// require('crypto');
 
 if (!String.prototype.normalize) {
-  require('unorm');
+  require("unorm");
+}
+
+let crypto;
+if (typeof window === "object") {
+  if (!window.crypto) window.crypto = {};
+  crypto = window.crypto;
+} else {
+  crypto = require("crypto");
+}
+
+if (!crypto.getRandomValues) {
+  crypto.getRandomValues = getRandomValues;
+}
+
+let randomBytes;
+
+function getRandomValues(arr) {
+  if (!randomBytes)
+    randomBytes = require("react-native-randombytes").randomBytes;
+
+  const bytes = randomBytes(arr.length);
+  for (var i = 0; i < bytes.length; i++) {
+    arr[i] = bytes[i];
+  }
 }
