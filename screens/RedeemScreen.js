e2f80104185a499a226fe470b69303b5975ec9a3
dekan <dekanbro@gmail.com>
2019-08-15 17:05:15 -0600

    work on flow

--- screens/RedeemScreen.js
+++ screens/RedeemScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Fragment } from "react";
+import React, { useEffect, useState, useContext, Fragment } from "react";
 import {
   Text,
   View,
@@ -6,18 +6,30 @@ import {
   TouchableOpacity,
   Clipboard,
   TextInput,
-  Button
+  Button,
+  Modal,
+  ActivityIndicator
 } from "react-native";
 import { Formik } from "formik";
 
 import CurrencyIndicator from "../components/CurrencyIndicator";
 import { globalStyles } from "../constants/styles";
 
-import config from "../config";
+import { CurrentWalletContext } from "../contexts/Store";
 
-export default function RedeemScreen() {
+import config from "../config";
+import useInterval from "../util/PollingUtil";
+
+export default function RedeemScreen(props) {
   const [link, setLink] = useState();
   const [cbRedeemLink, setCbRedeemLink] = useState("");
+  const [balanceWatch, setBalanceWatch] = useState();
+  const [balanceUpdated, setBalanceUpdated] = useState(false);
+  const [watchDelay, setWatchDelay] = useState(null);
+  const [watchCount, setWatchCount] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [currentWallet] = useContext(CurrentWalletContext);
 
   // get link in clipboad with param id=
   // validate right host
@@ -29,15 +41,27 @@ export default function RedeemScreen() {
   // on success watch balance for update, show loader with cancel button
   // on balance update show you have recieved 'value' confetti
 
+  useInterval(async () => {
+    console.log("watching,", balanceWatch, currentWallet.balance, watchCount);
+    setWatchCount(watchCount + 1);
+    if (+currentWallet.balance > +balanceWatch  || watchCount > 15) {
+      setBalanceUpdated(true);
+      setWatchDelay(null);
+      setCbRedeemLink("");
+    }
+  }, watchDelay);
+
   const getClipBoard = async () => {
     const clipboadData = await Clipboard.getString("redeemLinkHost");
     console.log("clipboadData", clipboadData);
-    setCbRedeemLink(clipboadData);
+    //if(clipboadData.indexOf(config.redeemLinkHost)){
+      setCbRedeemLink(clipboadData);
+    //}
   };
 
   const fetchLink = async link => {
     if (link) {
-      const linkId = clipboadData.split("?id=")[1];
+      const linkId = link.split("?id=")[1];
       let response = await fetch(`${config.apiUrl}links/get/${linkId}`);
       response = await response.json();
       console.log("*********************", response);
@@ -46,19 +70,89 @@ export default function RedeemScreen() {
     }
   };
 
-  // useEffect(() => {
-  //   fetchLink();
-  // }, []);
+  const redeemPut = async (linkId, redeemAddress) => {
+    console.log("redeem!", linkId, redeemAddress);
+
+    const putBody = {
+      linkId,
+      redeemAddress
+    };
+    let response = await fetch(`${config.apiUrl}links/redeem`, {
+      method: "put",
+      body: JSON.stringify(putBody)
+    });
+
+    return await response.json();
+  };
 
   return (
     <View style={globalStyles.container}>
-      <Text>LINK: {cbRedeemLink}</Text>
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(false);
+        }}
+      >
+        <View style={globalStyles.container}>
+          {!balanceUpdated ? (
+            <ActivityIndicator
+              style={{ marginTop: 20 }}
+              size="large"
+              color="#0000ff"
+            />
+          ) : (
+            <View style={globalStyles.container}>
+              <Text style={globalStyles.currencyHeading}>Redeemed DAI</Text>
+
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(false);
+                  props.navigation.navigate("Home");
+                }}
+              >
+                <View style={globalStyles.bigButtonView}>
+                <Image
+                  style={globalStyles.Icon}
+                  source={require("../assets/diamond.png")}
+                  resizeMode="contain"
+                />
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
+      </Modal>
+
       <Fragment>
         <Formik
           enableReinitialize
           initialValues={{ redeemLink: cbRedeemLink }}
-          onSubmit={async (values, { setSubmitting }) => {
-            console.log("redeem");
+          onSubmit={async (values, { setSubmitting, setStatus }) => {
+            console.log(
+              "redeem",
+              link.linkId,
+              currentWallet.sdk.state.account.address
+            );
+            setBalanceWatch(currentWallet.balance);
+            setModalVisible(true);
+            const redeemResponse = await redeemPut(
+              link.linkId,
+              currentWallet.sdk.state.account.address
+            );
+            //const redeemResponse = { success: true };
+            console.log("redeemResponse", redeemResponse);
+            if (!redeemResponse.error) {
+              setLink("");
+              setWatchDelay(1000);
+              
+            } else {
+              //errors.redeemLink = redeemResponse.error;
+              setModalVisible(false);
+              setStatus(redeemResponse.error)
+            }
+            //setSubmitting
           }}
           validate={values => {
             let errors = {};
@@ -70,38 +164,50 @@ export default function RedeemScreen() {
         >
           {props => (
             <View style={{ alignItems: "center", justifyContent: "center" }}>
-              <Text style={globalStyles.currencyHeading}>Redeem</Text>
-              <View style={globalStyles.inputRow}>
-                <TextInput
-                  style={{ border: 1, borderColor: "blue" }}
-                  value={props.values.redeemLink}
-                  onChangeText={props.handleChange("redeemLink")}
-                  onBlur={props.handleBlur("redeemLink")}
-                  maxLength={100}
-                />
-                <Text style={globalStyles.inputTextRight}>URL</Text>
-              </View>
-              {props.errors.redeemLink && (
-                <Text>! {props.errors.redeemLink}</Text>
-              )}
-              <Button onPress={() => getClipBoard()} title="check clipboad" />
-              <TouchableOpacity
-                onPress={() => fetchLink(props.values.redeemLink)}
-                disabled={props.isSubmitting}
-              >
-                <View style={globalStyles.bigButtonView}>
-                  <Image
-                    style={globalStyles.Icon}
-                    source={require("../assets/receive.png")}
-                    resizeMode="contain"
+              {link ? (
+                <View style={globalStyles.container}>
+                  <CurrencyIndicator
+                    label="You will redeem"
+                    amount={`${link.amount} DAI`}
                   />
-                </View>
-              </TouchableOpacity>
-              {link && (
-                <View>
-                  <CurrencyIndicator label="You will redeem" amount={` DAI`} />
+
                   <TouchableOpacity
                     onPress={props.handleSubmit}
+                    disabled={props.isSubmitting}
+                  >
+                    <View style={globalStyles.bigButtonView}>
+                      <Image
+                        style={globalStyles.Icon}
+                        source={require("../assets/receive.png")}
+                        resizeMode="contain"
+                      />
+                    </View>
+                  </TouchableOpacity>
+                {props.errors.redeemLink && <Text>{props.errors.redeemLink}</Text>}
+
+                </View>
+              ) : (
+                <View style={globalStyles.container}>
+                  <Text style={globalStyles.currencyHeading}>Redeem</Text>
+                  {cbRedeemLink ? (<Text>Link Imported</Text>) :
+                  (<Text>Import Link</Text>)}
+
+                  <TextInput
+                    style={{ left: -500 }}
+                    value={props.values.redeemLink}
+                    onChangeText={props.handleChange("redeemLink")}
+                    onBlur={props.handleBlur("redeemLink")}
+                    maxLength={100}
+                  />
+                  {props.errors.redeemLink && (
+                    <Text>{props.errors.redeemLink}</Text>
+                  )}
+                  <Button
+                    onPress={() => getClipBoard()}
+                    title="check clipboad"
+                  />
+                  <TouchableOpacity
+                    onPress={() => fetchLink(props.values.redeemLink)}
                     disabled={props.isSubmitting}
                   >
                     <View style={globalStyles.bigButtonView}>
