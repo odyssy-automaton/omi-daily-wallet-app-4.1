ae627fbec8806d3c6c60cbfe469224b1aec46392
dekan <dekanbro@gmail.com>
2019-08-14 21:44:08 -0600

    clean up buttons and header

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+
+import { Text, View, Image, TouchableOpacity } from "react-native";
 
 import { globalStyles } from "../constants/styles";
 
 export default function Header(props) {
   return (
     <View style={globalStyles.Header}>
-      <Text onPress={() => props.navigation.navigate("Home")}>
-        <Image 
+      <TouchableOpacity onPress={() => props.navigation.navigate("Home")}>
+        <Image
           source={require("../assets/DailyWallet__logo.png")}
-          style={{height: 25, width: 200}}
+          style={{ height: 25, width: 200 }}
           resizeMode="contain"
         />
-      </Text>
+      </TouchableOpacity>
       <Text
         style={globalStyles.HeaderText}
         onPress={() => props.navigation.navigate("Settings")}
