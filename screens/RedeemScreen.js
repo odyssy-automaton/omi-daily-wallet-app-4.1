d087b51a5668abcc896230b6420a3a1d01516c1e
dekan <dekanbro@gmail.com>
2019-08-15 12:11:55 -0600

    get data from clipboad

--- screens/RedeemScreen.js
+++ screens/RedeemScreen.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState, Fragment } from "react";
-import { Text, View, Image, TouchableOpacity, Clipboard } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Clipboard,
+  TextInput,
+  Button
+} from "react-native";
+import { Formik } from "formik";
+
 import CurrencyIndicator from "../components/CurrencyIndicator";
 import { globalStyles } from "../constants/styles";
 
@@ -7,6 +17,7 @@ import config from "../config";
 
 export default function RedeemScreen() {
   const [link, setLink] = useState();
+  const [cbRedeemLink, setCbRedeemLink] = useState("");
 
   // get link in clipboad with param id=
   // validate right host
@@ -18,14 +29,18 @@ export default function RedeemScreen() {
   // on success watch balance for update, show loader with cancel button
   // on balance update show you have recieved 'value' confetti
 
-  const fetchLink = async () => {
-    const clipboadData = Clipboard.getString();
+  const getClipBoard = async () => {
+    const clipboadData = await Clipboard.getString("redeemLinkHost");
+    console.log("clipboadData", clipboadData);
+    setCbRedeemLink(clipboadData);
+  };
 
-    if (clipboadData.indexOf(config.redeemLinkHost)) {
+  const fetchLink = async link => {
+    if (link) {
       const linkId = clipboadData.split("?id=")[1];
       let response = await fetch(`${config.apiUrl}links/get/${linkId}`);
       response = await response.json();
-      console.log('*********************', response);
+      console.log("*********************", response);
 
       setLink(response);
     }
@@ -37,35 +52,71 @@ export default function RedeemScreen() {
 
   return (
     <View style={globalStyles.container}>
+      <Text>LINK: {cbRedeemLink}</Text>
       <Fragment>
-        <CurrencyIndicator
-          label="You will redeem"
-          amount={` DAI`}
-        />
-        {!link ? (
-          <TouchableOpacity onPress={() => fetchLink()}>
-            <View style={globalStyles.bigButtonView}>
-              <Text>check</Text>
-
-              <Image
-                style={globalStyles.Icon}
-                source={require("../assets/receive.png")}
-                resizeMode="contain"
-              />
+        <Formik
+          enableReinitialize
+          initialValues={{ redeemLink: cbRedeemLink }}
+          onSubmit={async (values, { setSubmitting }) => {
+            console.log("redeem");
+          }}
+          validate={values => {
+            let errors = {};
+            if (!values.redeemLink) {
+              errors.redeemLink = "Required";
+            }
+            return errors;
+          }}
+        >
+          {props => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Text style={globalStyles.currencyHeading}>Redeem</Text>
+              <View style={globalStyles.inputRow}>
+                <TextInput
+                  style={{ border: 1, borderColor: "blue" }}
+                  value={props.values.redeemLink}
+                  onChangeText={props.handleChange("redeemLink")}
+                  onBlur={props.handleBlur("redeemLink")}
+                  maxLength={100}
+                />
+                <Text style={globalStyles.inputTextRight}>URL</Text>
+              </View>
+              {props.errors.redeemLink && (
+                <Text>! {props.errors.redeemLink}</Text>
+              )}
+              <Button onPress={() => getClipBoard()} title="check clipboad" />
+              <TouchableOpacity
+                onPress={() => fetchLink(props.values.redeemLink)}
+                disabled={props.isSubmitting}
+              >
+                <View style={globalStyles.bigButtonView}>
+                  <Image
+                    style={globalStyles.Icon}
+                    source={require("../assets/receive.png")}
+                    resizeMode="contain"
+                  />
+                </View>
+              </TouchableOpacity>
+              {link && (
+                <View>
+                  <CurrencyIndicator label="You will redeem" amount={` DAI`} />
+                  <TouchableOpacity
+                    onPress={props.handleSubmit}
+                    disabled={props.isSubmitting}
+                  >
+                    <View style={globalStyles.bigButtonView}>
+                      <Image
+                        style={globalStyles.Icon}
+                        source={require("../assets/receive.png")}
+                        resizeMode="contain"
+                      />
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              )}
             </View>
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity>
-            <View style={globalStyles.bigButtonView}>
-              <Text>redeem</Text>
-              <Image
-                style={globalStyles.Icon}
-                source={require("../assets/receive.png")}
-                resizeMode="contain"
-              />
-            </View>
-          </TouchableOpacity>
-        )}
+          )}
+        </Formik>
       </Fragment>
     </View>
   );
