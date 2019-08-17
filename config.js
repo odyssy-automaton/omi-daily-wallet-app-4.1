61e1e400a1d41d4e6eeb358adfee0b081d7bca35
dekan <dekanbro@gmail.com>
2019-08-16 19:01:17 -0600

    more redeem fun and test apk

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const envConfigs = {
     redeemLinkHost: "https://quirky-davinci-4da258.netlify.com"
   },
   production: {
-    apiUrl: "https://rx4y9fk2r8.execute-api.us-east-1.amazonaws.com/dev/",
-    redeemLinkHost: "https://quirky-davinci-4da258.netlify.com"
+    apiUrl: "https://qtet5hdaeb.execute-api.us-east-1.amazonaws.com/",
+    redeemLinkHost: "http://omi-daily-wallet.odyssy.io"
   }
 };
 const config = envConfigs[process.env["NODE_ENV"]];
