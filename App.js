1bcc68aa69b9a73358409d9c3edbfa9fa063363c
dekan <dekanbro@gmail.com>
2019-08-30 14:19:57 -0600

    move pincode to send only

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
-  StyleSheet,
-  Text,
   View,
-  TextInput,
-  ActivityIndicator
+
 } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import HomeScreen from "./screens/HomeScreen";
@@ -12,10 +9,7 @@ import RedeemScreen from "./screens/RedeemScreen";
 import SendScreen from "./screens/SendScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 import Header from "./components/Header";
-import CapturePin from "./components/CapturePin";
-import { globalStyles } from "./constants/styles";
 
-import * as Keychain from "react-native-keychain";
 
 import Store from "./contexts/Store";
 
@@ -40,65 +34,11 @@ const AppNavigator = createStackNavigator(
 const AppContainer = createAppContainer(AppNavigator);
 
 export default function App() {
-  const [loggedIn, setLogIn] = useState(false);
-  const [isLoading, setLoading] = useState(true);
-  const [isFirstTimePin, setFirstTimePin] = useState(true);
-  const [pin, setPin] = useState(null);
-  const handleSuccess = () => {
-    setLogIn(true);
-  };
-
-  const handleNewPin = async pin => {
-    await Keychain.setGenericPassword("userPin", pin);
-  };
-
-  useEffect(() => {
-    const checkPin = async () => {
-      try {
-        // Retreive the credentials
-
-        const credentials = await Keychain.getGenericPassword();
-        if (credentials) {
-          setFirstTimePin(false);
-          setPin(credentials.password);
-        } else {
-          setFirstTimePin(true);
-        }
-      } catch (error) {
-        console.log("Keychain couldn't be accessed!", error);
-      }
-
-      setLoading(false);
-    };
-
-    checkPin();
-  }, []);
 
   return (
     <Store>
       <View style={{ flex: 1 }}>
-        {loggedIn ? (
           <AppContainer />
-        ) : (
-          <CapturePin
-            pin={pin}
-            onSuccess={handleSuccess}
-            isFirstTimePin={isFirstTimePin}
-            handleNewPin={handleNewPin}
-          />
-        )}
-        {isLoading && (
-          <View
-            style={{
-              ...StyleSheet.absoluteFillObject,
-              backgroundColor: "rgba(255,255,255,.85)",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-          >
-            <ActivityIndicator size="large" />
-          </View>
-        )}
       </View>
     </Store>
   );
